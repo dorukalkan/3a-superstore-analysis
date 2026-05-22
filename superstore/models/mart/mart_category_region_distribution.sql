455e7762e5e3ad788e55f0886fcdaddaea181442
edaabilginn@gmail.com <edaabilginn@gmail.com>
2026-05-22 08:42:26 +0000

    fix duplicate explosion and add regional category distribution mart

--- superstore/models/mart/mart_category_region_distribution.sql
+++ superstore/models/mart/mart_category_region_distribution.sql
@@ -4,21 +4,28 @@ SELECT
 
     branch_region,
     branch_city,
-    branch_town,
+
+    category1,
+    category2,
 
     COUNT(DISTINCT order_id) AS total_orders,
 
-    SUM(amount) AS total_quantity_sold,
+    COUNT(DISTINCT item_id) AS unique_products,
+
+    SUM(amount) AS total_quantity,
 
     SUM(total_price) AS total_revenue,
 
-    AVG(total_price) AS avg_order_revenue
+    AVG(safe_total_basket) AS avg_basket_value
 
 FROM {{ ref('int_orderdetail_order_product_enriched') }}
 
 WHERE total_price > 0
 
 GROUP BY
+
     branch_region,
     branch_city,
-    branch_town
+
+    category1,
+    category2
