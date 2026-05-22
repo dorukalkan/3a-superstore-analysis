e16a2167424b1ff624e124e10d8544387eddb0a7
Doruk Alkan <dorukalkan1.0@gmail.com>
2026-05-22 12:30:43 +0300

    change any_value() to min()

--- superstore/models/intermediate/int_branch_dim.sql
+++ superstore/models/intermediate/int_branch_dim.sql
@@ -11,9 +11,9 @@ branch_dim as (
 
     select
         branch_id,
-        any_value(region) as region,
-        any_value(city) as city,
-        any_value(branch_town) as branch_town,
+        min(region) as region,
+        min(city) as city,
+        min(branch_town) as branch_town,
         count(distinct town) as covered_town_count
     from branch_coverage
     group by branch_id
